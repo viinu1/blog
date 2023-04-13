170884c741d5e226e7e474918b94f9ccbba71512
Vĩ Phạm Văn <phamviabc2@gmail.com>
2023-04-13 17:33:18 +0700

    new project

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const path =require('path')
+const express = require("express");
+const morgan = require("morgan");
+const {engine} = require ('express-handlebars')
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// http logger
+app.use(morgan("combined"));
+
+// template engine
+app.engine('hbs',engine({
+  extname:'.hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views',path.join(__dirname,'resources/views'));
+
+
+
+// app
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
+app.get("/view", (req, res) => {
+  res.render('view');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
